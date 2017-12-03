0dcdf533488185d977aeb65dbf3f75a541462158
Mateusz <lesniak.mat@gmail.com>
2017-12-03 12:59:42 +0100

    Add comments creator

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,17 @@
 class CommentsController < ApplicationController
+
   def create
     binding.pry
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to article_path(@comment.article_id)
+    else
+      redirect_to articles_path
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :article_id)
   end
 end
