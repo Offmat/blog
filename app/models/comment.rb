ea1e06934779bfb53e11e68f415bcd7954d4f6fa
Mateusz <lesniak.mat@gmail.com>
2017-12-23 18:53:47 +0100

    Change email validation message

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
-  validates :commenter, presence: true, email: { message: 'Własna wiadomość o błędzie z emaila' }
+  validates :commenter, presence: true, email: { message: nil }
   validates :body, presence: true, length: { in: 6..500 }
 end
