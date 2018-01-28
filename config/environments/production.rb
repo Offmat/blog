f539ddd99523c35ae262ad3b8e250a05675d40da
Mateusz <lesniak.mat@gmail.com>
2018-01-28 10:35:31 +0100

    Fix localhost z puzzle_room to blog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "blog_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'enigmatic-caverns-15394.herokuapp.com', port: 80 }
+  config.action_mailer.default_url_options = { host: 'agile-brushlands-69915.herokuapp.com', port: 80 }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
@@ -73,7 +73,7 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
-  
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
