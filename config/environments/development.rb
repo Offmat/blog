2d51d998f2f0eb5a561da02d2954144c6fada179
Mateusz <lesniak.mat@gmail.com>
2018-01-28 10:26:54 +0100

    Send mail with sandgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,10 +29,23 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :domain => 'herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
