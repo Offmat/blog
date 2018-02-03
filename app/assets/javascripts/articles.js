70fe358783b6e54f30005ee209ab9c545d41adab
Mateusz <lesniak.mat@gmail.com>
2018-02-03 14:42:27 +0100

    Add collons do JS code

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,13 +1,14 @@
-window.addEventListener( "load", function() {
-  var button = document.getElementById("comments_btn")
-  var comments = document.getElementById("comments")
+
+window.addEventListener("load", function () {
+  var button = document.getElementById("comments_btn");
+  var comments = document.getElementById("comments");
   button.addEventListener( "click", function() {
     if (button.innerText == "Show comments") {
-      button.innerText = "Hide comments"
-      comments.setAttribute("style", "display: block")
+      button.innerText = "Hide comments";
+      comments.setAttribute ("style", "display: block");
     } else {
-      button.innerText = "Show comments"
-      comments.setAttribute("style", "display: none")
+      button.innerText = "Show comments";
+      comments.setAttribute("style", "display: none");
     }
-  })
-})
+  });
+});
